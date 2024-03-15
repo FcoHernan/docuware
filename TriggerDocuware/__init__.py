f2ce65ba44858b01b7751562a2f16efa3fa768ee
Francisco Ramirez <FranciscoHRF@gmail.com>
2024-03-15 17:06:04 -0300

    terminando de cargar

--- TriggerDocuware/__init__.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-from APP import ExtraeDocuware
- 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-   
-    req_body = req.get_json()
-    ExtraeDocuware.getOrganization(req_body) #modificar cuando tenga la funcion
- 
-    return func.HttpResponse(f"This HTTP triggered function executed successfully.")
--- TriggerElina/__init__.py
+++
@@ -1,9 +0,0 @@
-import azure.functions as func
-from APP import ExtraeElina
- 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-   
-    req_body = req.get_json()
-    ExtraeElina.Func_Extrae_Elina(req_body)
- 
-    return func.HttpResponse(f"This HTTP triggered function executed successfully.")
